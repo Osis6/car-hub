06c15c92b63730598f2fde8e1f62d533460d7c8c
BOUNOU <djeey.osis@gmail.com>
2024-02-27 23:43:42 +0100

    hero carcard sections

---
+++ components/index.ts
@@ -0,0 +1,8 @@
+/** @format */
+
+import CarCard from './CarCard';
+import Footer from './Footer';
+import NavBar from './Navbar';
+import Hero from './Hero';
+
+export { Hero, CarCard, Footer, NavBar };
