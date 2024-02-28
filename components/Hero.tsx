b81433408a55f9b7dad6421982c2856367812c01
BOUNOU <djeey.osis@gmail.com>
2024-02-28 02:11:03 +0100

    some styles

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -36,7 +36,7 @@ const Hero = () => {
         </div>
         <div className='hero__image-container'>
           <div className='hero__image'>
-            <Image src='/Hero.png' alt='hero' fill className='object-contain' />
+            <Image src='/car.svg' alt='hero' fill className='object-contain' />
           </div>
 
           <div className='hero__image-overlay' />
